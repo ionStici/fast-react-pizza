1efc0c859a2546b0ea3ef31628ca9b6f037cb8f8
Ion Stici <ionsticidev@gmail.com>
2024-05-27 06:47:19 +0300

    Setup project 'after tailwind'

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -4,11 +4,10 @@ import MenuItem from './MenuItem';
 
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
 
   return (
-    <ul>
-      {menu.map(pizza => (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
     </ul>
